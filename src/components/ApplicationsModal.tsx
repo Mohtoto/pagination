4c0b274cbc9a8a34b9f47e763ff9dde23efd4b81
Moh Totonchy <mohtotonchy@Nadas-MacBook-Air.local>
2025-06-17 18:31:25 +0800

    initial commit

--- src/components/ApplicationsModal.tsx
+++ src/components/ApplicationsModal.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ApplicationsModal = () => {
   return (
-    <div className='flex justify-center items-center bg-red-500 h-[200px] w-[300px]'>
+    <div className='absolute top-2/4 left-2/4 flex justify-center items-center bg-red-500 h-[200px] w-[300px]'>
         <h1 className='text-white font-bold text-2xl'>modal header</h1>
     </div>
   )
